00792cd332aeda8e9825a3365bee4c964eac3655
sachin <sachinrkeche@gmail.com>
2016-09-12 00:31:48 +0530

    Initial commit

---
+++ app/src/main/java/technology/patil/keche/sachin/testapplication/First_Fragment.java
@@ -0,0 +1,20 @@
+package technology.patil.keche.sachin.testapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by sachin on 11/9/16.
+ */
+public class First_Fragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.first_fragment,container,false);
+    }
+}
